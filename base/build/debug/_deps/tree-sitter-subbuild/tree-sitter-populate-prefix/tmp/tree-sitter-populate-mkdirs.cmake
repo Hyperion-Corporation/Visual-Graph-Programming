b0c82433173a2227d8cdea7f777abac6c680f3ca
fafpinheiro <fredpinheiro@sapo.pt>
2026-07-06 00:07:18 +0100

    Import llvm, treesitter vendors and cmake config

---
+++ base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/tmp/tree-sitter-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-src")
+  file(MAKE_DIRECTORY "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-src")
+endif()
+file(MAKE_DIRECTORY
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-build"
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix"
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/tmp"
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/src/tree-sitter-populate-stamp"
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/src"
+  "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/src/tree-sitter-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/src/tree-sitter-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/DEVELOPMENT/Visual-Graph-Programming/base/build/debug/_deps/tree-sitter-subbuild/tree-sitter-populate-prefix/src/tree-sitter-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
